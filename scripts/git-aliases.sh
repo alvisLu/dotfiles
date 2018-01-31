13697e536e23d348da4415604295cf549bda9e89
Alvis Lu <alvislu@mapacode.tw>
2018-01-31 21:59:05 +0800

    加入 undo last difflast commit -m

--- scripts/git-aliases.sh
+++ scripts/git-aliases.sh
@@ -5,4 +5,9 @@
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
+git config --global alias.cm "commit -m"
 git config --global alias.st status
+
+git config --global alias.undo "reset --soft HEAD~1"
+git config --global alias.last "log -1 HEAD"
+git config --global alias.difflast "diff --cached HEAD^"
