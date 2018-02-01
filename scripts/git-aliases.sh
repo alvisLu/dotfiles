adaf69d759810dbb90546cd8c49447b2620e5a94
Alvis Lu <alvislu@mapacode.tw>
2018-02-01 13:52:43 +0800

    加入顯示與使用 nvim 編輯

--- scripts/git-aliases.sh
+++ scripts/git-aliases.sh
@@ -11,3 +11,7 @@ git config --global alias.st status
 git config --global alias.undo "reset --soft HEAD~1"
 git config --global alias.last "log -1 HEAD"
 git config --global alias.difflast "diff --cached HEAD^"
+# 中文顯示
+git config --global core.quotepath false
+
+git config --global core.editor "nvim"
