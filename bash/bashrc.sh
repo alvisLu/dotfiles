ddb09a2f549c392df76fbd3e9310be77eea4e3dc
Alvis Lu <alvislu@mapacode.tw>
2018-04-11 11:56:09 +0800

    Added nvim environmet variable.

--- bash/bashrc.sh
+++ bash/bashrc.sh
@@ -13,12 +13,14 @@ alias ll='ls -lXr'
 alias la='ls -laXr'
 alias cp='cp -iv'
 alias mv='mv -iv'
-alias rm='rm -vrf'
+alias rm='rm -rf'
 
 export PATH=$HOME/bin:$PATH
-
-# tmux
 export TERM="screen-256color"
+export EDITOR="nvim"
+# gem
+export PATH=$PATH:/root/.gem/ruby/2.5.0/bin
+
 # nvm
 source /usr/share/nvm/init-nvm.sh
 # FZF
@@ -28,4 +30,3 @@ source /usr/share/fzf/key-bindings.bash
 source $HOME/git/linux-environ-config/scripts/fzf-git.sh
 source $HOME/git/linux-environ-config/scripts/git-completion.bash
 
-export PATH=$PATH:/root/.gem/ruby/2.5.0/bin
