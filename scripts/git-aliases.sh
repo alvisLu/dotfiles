15d6f62ed6dbdaf3836eedc1160005a04dd7b4cc
Alvis Lu <alvislu@mapacode.tw>
2018-10-24 14:16:27 +0800

    update formate

--- scripts/git-aliases.sh
+++ scripts/git-aliases.sh
@@ -19,7 +19,7 @@ git config --global alias.tree2 "log --graph --abbrev-commit --decorate "\
 "--pretty=oneline"
 
 git config --global alias.tree "log --graph --abbrev-commit --decorate "\
-"--format=format:'%C(bold yellow)%h %C(dim cyan)%cd %Creset%C(white)%s"\
+"--format=format:'%C(bold yellow)%h %C(dim cyan)%cd %C(auto)%s %Creset"\
 "%C(dim white) (%an) %C(auto)%d' --date=format:%Y-%m-%d"
 
 
