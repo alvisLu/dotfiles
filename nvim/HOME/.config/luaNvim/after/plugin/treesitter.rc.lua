630c34f3125541f71b9e97cf5d3dc8bd95d0f529
Alvis Lu <alvis@simpos.com.tw>
2023-01-02 13:02:18 +0800

    1. add fold setting, 2. modify treesitter ensure_installed

--- nvim/HOME/.config/luaNvim/after/plugin/treesitter.rc.lua
+++ nvim/HOME/.config/luaNvim/after/plugin/treesitter.rc.lua
@@ -11,17 +11,18 @@ ts.setup {
     disable = {},
   },
   ensure_installed = {
+    "javascript",
+    "typescript",
     "tsx",
     "toml",
-    "fish",
-    "php",
     "json",
     "yaml",
-    "swift",
     "css",
     "html",
+    "fish",
     "lua"
   },
+
   autotag = {
     enable = true,
   },
@@ -30,6 +31,36 @@ ts.setup {
     enable_autocmd = true
   }
 }
+
+
+-- fold
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldenable = false
+
+
+local M = {}
+-- function to create a list of commands and convert them to autocommands
+-------- REF: https://www.jmaguire.tech/posts/treesitter_folding/
+function M.nvim_create_augroups(definitions)
+  for group_name, definition in pairs(definitions) do
+    vim.api.nvim_command('augroup ' .. group_name)
+    vim.api.nvim_command('autocmd!')
+    for _, def in ipairs(definition) do
+      local command = table.concat(vim.tbl_flatten { 'autocmd', def }, ' ')
+      vim.api.nvim_command(command)
+    end
+    vim.api.nvim_command('augroup END')
+  end
+end
+
+local autoCommands = {
+  -- other autocommands
+  open_folds = {
+    { "BufReadPost,FileReadPost", "*", "normal zR" }
+  }
+}
+M.nvim_create_augroups(autoCommands)
 
 local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
 parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
