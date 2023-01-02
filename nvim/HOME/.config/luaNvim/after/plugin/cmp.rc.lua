245e44b9e77456e42ea2b31779fb14ce4dcbdf40
Alvis Lu <alvis@simpos.com.tw>
2023-01-02 21:48:07 +0800

    add js snippets to ts

--- nvim/HOME/.config/luaNvim/after/plugin/cmp.rc.lua
+++ nvim/HOME/.config/luaNvim/after/plugin/cmp.rc.lua
@@ -6,7 +6,9 @@ local snip_status_ok, luasnip = pcall(require, "luasnip")
 if not snip_status_ok then
   return
 end
+
 require("luasnip/loaders/from_vscode").lazy_load()
+luasnip.filetype_extend("typescript", { "javascript" })
 
 local check_backspace = function()
   local col = vim.fn.col "." - 1
