58462976c0fb153ba7fdef95c45aa248ad660e5c
Alvis Lu <uualvislu@gmail.com>
2025-01-15 18:24:51 +0800

    update lazyNvim

--- nvim/HOME/.config/lazyNvim/lua/plugins/coding.lua
+++ nvim/HOME/.config/lazyNvim/lua/plugins/coding.lua
@@ -1,8 +1,34 @@
 return {
+  -- https://www.lazyvim.org/extras/coding/luasnip
   {
     "L3MON4D3/LuaSnip",
     keys = function()
       return {}
     end,
+  },
+  {
+    "rafamadriz/friendly-snippets",
+    -- config = function()
+    --   require("luasnip.loaders.from_vscode").lazy_load()
+    --   require("luasnip.loaders.from_vscode").lazy_load({ paths = { vim.fn.stdpath("config") .. "/snippets" } })
+    -- end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    optional = true,
+    dependencies = { "saadparwaiz1/cmp_luasnip" },
+    opts = function(_, opts)
+      opts.snippet = {
+        expand = function(args)
+          require("luasnip").lsp_expand(args.body)
+        end,
+      }
+      table.insert(opts.sources, { name = "luasnip" })
+    end,
+  -- stylua: ignore
+  keys = {
+    { "<tab>", function() require("luasnip").jump(1) end, mode = "s" },
+    { "<s-tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
+  },
   },
 }
---
+++ nvim/HOME/.config/lazyNvim/lua/plugins/copilot.lua
@@ -0,0 +1,52 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    event = "InsertEnter",
+    opts = {
+      suggestion = {
+        enabled = not vim.g.ai_cmp,
+        auto_trigger = true,
+        keymap = {
+          accept = false, -- handled by nvim-cmp / blink.cmp
+          next = "<M-]>",
+          prev = "<M-[>",
+        },
+      },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = true,
+      },
+    },
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    opts = {},
+    config = function(_, opts)
+      local copilot_cmp = require("copilot_cmp")
+      copilot_cmp.setup(opts)
+      -- attach cmp source whenever copilot attaches
+      -- fixes lazy-loading issues with the copilot cmp source
+      LazyVim.lsp.on_attach(function()
+        copilot_cmp._on_insert_enter({})
+      end, "copilot")
+    end,
+    specs = {
+      {
+        "hrsh7th/nvim-cmp",
+        optional = true,
+        -- @param opts cmp.ConfigSchema
+        opts = function(_, opts)
+          table.insert(opts.sources, 1, {
+            name = "copilot",
+            group_index = 1,
+            priority = 100,
+          })
+        end,
+      },
+    },
+  },
+  { "giuxtaposition/blink-cmp-copilot" },
+}
